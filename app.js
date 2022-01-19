8047377b798472d8e6dd756482c4f970574d2a1c
Alen <lukac.alen@gmail.com>
2022-01-19 21:05:45 +0100

    update

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const SolarEdgeModbusClient = require("solaredge-modbus-client");
 const mqtt = require("mqtt");
 const { universalParseValue, allMetrics } = require("./universalMetricParser");
 
+axios.defaults.timeout = 6000;
+
 var app = express();
 app.use(apiMetrics());
 
@@ -20,12 +22,12 @@ var client2 = mqtt.connect(process.env.MQTT_ADDRESS, {
   username: process.env.MQTT_USERNAME, //TODO better
   password: process.env.MQTT_PASSWORD,
 });
-/*
-let solar = new SolarEdgeModbusClient({
+
+var solar = new SolarEdgeModbusClient({
   host: process.env.SOLAREDGE_ADDRESS,
-  port: 502,
+  port: 1502,
 });
-*/
+
 client2.on("connect", function () {
   console.log("Mqtt2 connected");
   client2.subscribe("current/#", function (err) {
@@ -35,17 +37,8 @@ client2.on("connect", function () {
   });
 });
 
-(async () => {
-  while (true) {
-    await monitorMainWattmetter();
-    await monitorEvse();
-    //await monitorSolarEdge();
-    setEvseChargingCurrent();
-  }
-})();
-
 client2.on("message", function (topic, message) {
-  console.log("Got mqtt message "+message.toString() + " on topic "+ topic);
+  console.log("Got mqtt message on topic "+ topic);
   var splitTopic = topic.split("/");
   var deviceId = splitTopic[1];
   var valueRaw = message.toString();
@@ -54,14 +47,59 @@ client2.on("message", function (topic, message) {
   universalParseValue(valueRaw, metricName, deviceId);
 });
 
+var lastSetCurrent = 0;
+
+function setEvseChargingCurrent() {
+  console.log("Setting charging current")
+  var chargingCurrent = allMetrics["evse01_amp_0"] / 1000;
+  var maxFuse = 25;
+  
+  var phasePower = [
+                  allMetrics["watt01_I1_0"] / 100.0 - chargingCurrent - (allMetrics["solar01_I_AC_CurrentA_0"]/100), 
+                  allMetrics["watt01_I2_0"] / 100.0 - chargingCurrent - (allMetrics["solar01_I_AC_CurrentB_0"]/100), 
+                  allMetrics["watt01_I3_0"] / 100.0 - chargingCurrent - (allMetrics["solar01_I_AC_CurrentC_0"]/100)
+                ];
+  var highestCurrent = maxFuse - Math.round(Math.max(...phasePower)); //subtract current now
+  if (highestCurrent < 0) {
+    highestCurrent = 0;
+  }
+  
+  if(Math.abs(lastSetCurrent - highestCurrent) > 2){
+    var setCurrent = highestCurrent
+    if(setCurrent > 32){
+      setCurrent = 32;
+    }
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        console.log("Setting charge speed")
+        axios
+          .get(process.env.OEVSE_ADDRESS+"/r?json=1&rapi=$SC+" + setCurrent)
+          .then(function (response) {
+            lastSetCurrent = highestCurrent
+            console.log("Max charing current set to "+highestCurrent+"A per phase")
+            universalParseValue(setCurrent, "set_max_charge_current", "wattCalc01");
+            console.log(response.data);
+          })
+          .catch(function (error) {
+            console.log(error.toJSON());
+          })
+          .finally(function () {
+            res();
+          });
+      }, 10);
+    });
+  }
+}
+
 function monitorMainWattmetter() {
   return new Promise((res, rej) => {
     setTimeout(() => {
+      console.log("Getting wattmeter data")
       axios
         .get(process.env.WATT_ADDRESS+"/updateData")
         .then(function (response) {
           delete response.data.WATTMETER_TIME;
-          console.log("Got wattmetter message "+JSON.stringify(response.data));
+          //console.log("Got wattmetter message "+JSON.stringify(response.data));
           for (const property in response.data) {
             var deviceId = "watt01";
             var metricName = deviceId + "_" + property;
@@ -70,51 +108,24 @@ function monitorMainWattmetter() {
           }
         })
         .catch(function (error) {
-          console.log(error);
+          console.log(error.toJSON());
         })
-        .then(function () {
+        .finally(function () {
           res();
         });
-    }, 2000);
+    }, 500);
   });
-}
-
-var lastSetCurrent = 0;
-
-function setEvseChargingCurrent() {
-  var chargingCurrent = allMetrics["evse01_amp_0"] / 1000;
-  var maxFuse = 25;
-  var phasePower = [allMetrics["watt01_I1_0"] / 100.0 - chargingCurrent, allMetrics["watt01_I2_0"] / 100.0 - chargingCurrent, allMetrics["watt01_I3_0"] / 100.0 - chargingCurrent];//TODO include solar data
-  var highestCurrent = maxFuse - Math.round(Math.max(...phasePower)); //subtract current now
-  if (highestCurrent < 0) {
-    highestCurrent = 0;
-  }
-  
-  if(Math.abs(lastSetCurrent - highestCurrent) > 1){
-    axios
-    .get(process.env.OEVSE_ADDRESS+"/r?json=1&rapi=$SC+" + highestCurrent)
-    .then(function (response) {
-      lastSetCurrent = highestCurrent
-      console.log("Max charing current set to "+highestCurrent+"A per phase")
-      console.log(response.data);
-    })
-    .catch(function (error) {
-      console.log(error); //raise error
-    })
-  }
-    
-  universalParseValue(lastSetCurrent, "set_max_charge_current", "wattCalc01");
-
 }
 
 function monitorEvse() {
   return new Promise((res, rej) => {
     setTimeout(() => {
+      console.log("Getting evse data")
       axios
         .get(process.env.OEVSE_ADDRESS+"/status")
         .then(function (response) {
           delete response.data.time;
-          console.log("Got ovms message"+JSON.stringify(response.data));
+          //console.log("Got ovms message"+JSON.stringify(response.data));
           for (const property in response.data) {
             var deviceId = "evse01";
             var metricName = deviceId + "_" + property;
@@ -124,34 +135,58 @@ function monitorEvse() {
           }
         })
         .catch(function (error) {
-          console.log(error);
+          console.log(error.toJSON());
         })
-        .then(function () {
+        .finally(function () {
           res();
         });
-    }, 2000);
+    }, 500);
   });
 }
 
 function  monitorSolarEdge(){
   return new Promise((res, rej) => {
+    var didGetData = false;
+
     setTimeout(() => {
+      if(didGetData == false){
+        res();
+        process.exit(1);
+      }
+    }, 5000);
+
+    setTimeout(() => {
+      console.log("Getting solaredge data")
       solar
         .getData()
         .then((data) => {
-          console.log(data); //todo export dis
+          //console.log("Got solaredge message "+JSON.stringify(data));
+          didGetData = true;
           var deviceId = "solar01";
-          var metricName = deviceId + "_" + property;
-          var valueRaw = response.data[property];
-
-          universalParseValue(valueRaw, metricName, deviceId);
+          for (var i=0;i<data.length;i++) {
+            var metricName = deviceId + "_" + data[i].name;
+            var valueRaw = data[i].value;
+            universalParseValue(valueRaw, metricName, deviceId);
+          }
         })
         .catch(function (error) {
           console.log(error);
-        });
-    }, 5000);
+        })
+        .finally(function () {
+          res();
+        });;
+    }, 500);
   });
 }
+
+(async () => {
+  while (true) {
+    await monitorMainWattmetter();
+    await monitorEvse();
+    await monitorSolarEdge();
+    await setEvseChargingCurrent();
+  }
+})();
 
 console.log("App running on port 3000");
 module.exports = app;
